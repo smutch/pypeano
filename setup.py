9698aec69b63168fe04be7d72dabe836d2c4fbd5
Simon Mutch <smutch@unimelb.edu.au>
2017-11-18 20:27:45 +1100

    Add numba to dependencies list.

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="pypeano",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
-    setup_requires=["cffi>=1.0.0", "pytest-runner"],
+    setup_requires=["cffi>=1.0.0",
+                    "pytest-runner",
+                    "numba>=0.35.0"],
     tests_require=["pytest"],
     include_package_data=True,
     cffi_modules=["peano/peano_build.py:ffibuilder"],
-    install_requires=["cffi>=1.10.0", "numpy>=1.13.3"],
+    install_requires=["cffi>=1.10.0", "numpy>=1.13.3", "numba>=0.35.0"],
 )
