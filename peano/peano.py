1574052247b2c6c125d9cb9960dbca7aaff9c95a
Simon Mutch <smutch@unimelb.edu.au>
2017-11-18 20:25:42 +1100

    Add numba driven vectorization and fast calculation on arrays

--- peano/peano.py
+++ peano/peano.py
@@ -1,17 +1,46 @@
-from numpy import int64
-from ._peano import lib, ffi
+import numba as nb
+from numba import cffi_support
+from numpy import array, zeros
+from . import _peano
+
+c__peano_hilbert_key = _peano.lib.peano_hilbert_key
+c__peano_hilbert_key_inverse = _peano.lib.peano_hilbert_key_inverse
+ffi = _peano.ffi
+cffi_support.register_module(_peano)
 
 
-def peano_hilbert_key(x: int, y: int, z: int, bits: int):
-    return lib.peano_hilbert_key(x, y, z, bits)
+@nb.jit(nopython=True)
+def peano_hilbert_key(x, y, z, bits):
+    return c__peano_hilbert_key(x, y, z, bits)
 
 
-def peano_hilbert_key_inverse(key: int64, bits: int):
-    x, y, z = (ffi.new("int *") for ii in range(3))
-    x[0], y[0], z[0] = (-999 for ii in range(3))
-    _key = ffi.cast("long long", key)
+@nb.vectorize(nopython=True)
+def peano_hilbert_keys(x, y, z, bits):
+    return peano_hilbert_key(x, y, z, bits)
 
-    lib.peano_hilbert_key_inverse(_key, bits, x, y, z)
-    assert x[0] != -999 and y[0] != -999 and z[0] != -999
+
+@nb.jit(nopython=True)
+def peano_hilbert_key_inverse(key, bits):
+    x = array([-999], nb.int32)
+    y = array([-999], nb.int32)
+    z = array([-999], nb.int32)
+
+    c__peano_hilbert_key_inverse(nb.longlong(key), bits, ffi.from_buffer(x),
+                                 ffi.from_buffer(y), ffi.from_buffer(z))
 
     return x[0], y[0], z[0]
+
+
+@nb.jit(nopython=True)
+def peano_hilbert_keys_inverse(keys, bits):
+    x = zeros(keys.size, nb.int32)
+    y = zeros(keys.size, nb.int32)
+    z = zeros(keys.size, nb.int32)
+
+    for arr in x, y, z:
+        arr[:] = -999
+
+    for ii, key in enumerate(keys):
+        x[ii], y[ii], z[ii] = peano_hilbert_key_inverse(key, bits)
+
+    return x, y, z
--- tests/test_peano.py
+++ tests/test_peano.py
@@ -1,4 +1,6 @@
-from peano import peano_hilbert_key, peano_hilbert_key_inverse
+import numpy as np
+from peano import peano_hilbert_key, peano_hilbert_keys
+from peano import peano_hilbert_key_inverse, peano_hilbert_keys_inverse
 
 
 def test_zero():
@@ -8,3 +10,12 @@ def test_zero():
     assert x == 0
     assert y == 0
     assert z == 0
+
+
+def test_zeros():
+    x, y, z = (np.zeros(50, np.int) for ii in range(3))
+    assert np.all(peano_hilbert_keys(x, y, z, 3) == 0)
+
+    keys = np.zeros(50, np.longlong)
+    res = peano_hilbert_keys_inverse(keys, 3)
+    assert np.all(r == 0 for r in res)
